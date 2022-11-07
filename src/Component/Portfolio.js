f8a710fd142f017205160ad28a6b6b73c6752a43
Zakaria <zakariazainuddin41@yahoo.com>
2022-11-07 22:30:59 +0800

    sec

--- src/Component/Portfolio.js
+++ src/Component/Portfolio.js
@@ -2,7 +2,7 @@ import React from 'react'
 import work1 from "../Assets/work-1.png"
 import work2 from "../Assets/work-2.png"
 import work3 from "../Assets/work-3.png"
-import bravepic from '../Assets/bravepic.PNG'
+import bravepic from '../Assets/brave.PNG'
 import projectpic from '../Assets/projpic2.PNG'
 import projectpic2 from '../Assets/projpic3.PNG'
 import try2 from '../Assets/mywork.png'
@@ -12,37 +12,37 @@ import classes from '../UI/AnimationImg.module.css'
 
 function Portfolio() {
   return (
-    <div id='portfolio' className=' m-auto max-w-max mt-10 space-x-0'>
-      <h1 className='text-4xl font-bold mb-10 text-red-500 text-center md:text-left' >My Project</h1>
-        <div className="container flex md:space-x-10 flex-col md:flex-row items-center">
+    <div id='portfolio' className=' m-auto max-w-max mt-10'>
+      <h1 className='text-4xl font-bold mb-10 text-red-500 text-center md:text-left' >My Projects</h1>
+        <div className="container flex md:space-x-10 flex-col md:flex-row items-center ">
         <div className={classes.container1}>
-            <img src={projectpic} alt="work1" className='max-w-xs h-72' />
+            <img src={projectpic} alt="work1" className='w-auto h-72' />
             <AnimationImg title="My Portfolio Website" description="(React,Tailwind CSS)"
             landingPage={'https://zakariazainuddin.netlify.app/'}/>
         </div>
         <div className={classes.container1}>
-            <img src={bravepic} alt="work1" className='max-w-xs h-72' />
+            <img src={bravepic} alt="work1" className='w-auto h-72' />
             <AnimationImg title="Brave Or Coward" description="Whoever gets the 100 points first wins !" software="(Html,Css,Javascript)" 
             landingPage={"https://brave-or-coward.netlify.app/"} github={'https://github.com/zacky791/Brave-Or-Coward-Game'}/>
         </div>
-        <div className={classes.container1}>
+        {/* <div className={classes.container1}>
             <img src={projectpic2} alt="work1" className='max-w-xs h-72' />
             <AnimationImg title="GrabFood" description="Description"/>
-        </div>
+        </div> */}
       </div>
       <div className="container flex md:space-x-10 flex-col md:flex-row items-center">
         <div className={classes.container1}>
-            <img src={work1} alt="work1" className='max-w-xs' />
-            <AnimationImg title="Title" description="Description"/>
+            <img src={projectpic2} alt="work1" className='w-auto h-72' />
+            <AnimationImg title="GrabFood" description="Description"/>
         </div>
         <div className={classes.container1}>
-            <img src={work2} alt="work1" className='max-w-xs' />
+            <img src={projectpic} alt="work1" className='w-auto h-72' />
             <AnimationImg title="Title" description="Description"/>
         </div>
-        <div className={classes.container1}>
+        {/* <div className={classes.container1}>
             <img src={work3} alt="work1" className='max-w-xs' />
             <AnimationImg title="Title" description="Description"/>
-        </div>
+        </div> */}
       </div>
     </div>
   )
