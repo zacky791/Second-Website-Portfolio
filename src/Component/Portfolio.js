2214e5cc76f5678afa0f9bc0edb34783b60f0013
Zakaria <zakariazainuddin41@yahoo.com>
2022-11-12 12:32:08 +0800

    update project

--- src/Component/Portfolio.js
+++ src/Component/Portfolio.js
@@ -1,12 +1,13 @@
 import React from 'react'
-import bravepic from '../Assets/brave.PNG'
+// import bravepic from '../Assets/brave.PNG'
 import projectpic from '../Assets/projpic2.PNG'
 import projectpic2 from '../Assets/projpic3.PNG'
 import AnimationImg from '../UI/AnimationImg'
 import classes from '../UI/AnimationImg.module.css'
 import omnifood from '../Assets/omnifood.PNG' 
-import maybank from '../Assets/maybank.PNG'
 import spotify from '../Assets/spotify.PNG'
+import ai from "../Assets/ai.PNG"
+import zackbank from '../Assets/zackbankpic.PNG'
 
 
 function Portfolio() {
@@ -16,36 +17,33 @@ function Portfolio() {
         <div className="container flex md:space-x-10 flex-col md:flex-row items-center ">
         <div className={classes.container1}>
             <img src={omnifood} alt="" className='w-auto h-72' />
-            <AnimationImg title="Omni food" description="" software="Html,Css,Javascript"
+            <AnimationImg title="Omni Food" description="" software="Html,Css,Javascript"
             landingPage={'https://omni-foodss.netlify.app/'} github={'https://github.com/zacky791/Omni-Food-'}/>
         </div>
         <div className={classes.container1}>
-            <img src={maybank} alt="" className='w-auto h-72' />
-            <AnimationImg title="Maybank" description="" software="React,Tailwind CSS" 
-            landingPage={""} github={''}/>
+            <img src={ai} alt="" className='w-auto h-72' />
+            <AnimationImg title="Artificial Intellegance" description="" software="React,CSS" 
+            landingPage={"https://artificial-intelligence3.netlify.app/"} github={'https://github.com/zacky791/Artificial-Intelligence'}/>
         </div>
-        {/* <div className={classes.container1}>
-            <img src={projectpic2} alt="work1" className='max-w-xs h-72' />
-            <AnimationImg title="GrabFood" description="Description"/>
-        </div> */}
       </div>
       <div className="container flex md:space-x-10 flex-col md:flex-row items-center">
       <div className={classes.container1}>
             <img src={projectpic} alt="" className='w-auto h-72' />
-            <AnimationImg title="My Portfolio" description="Description" software='React,Tailwind CSS'
+            <AnimationImg title="My Portfolio" description="" software='React,Tailwind CSS'
             landingPage={'https://zakariazainuddin.netlify.app/'} github={''}
             />
         </div>
         <div className={classes.container1}>
-            <img src={projectpic2} alt="" className='w-auto h-72' />
-            <AnimationImg title="GrabFood" description="Description"/>
+            <img src={zackbank} alt="" className='w-auto h-72' />
+            <AnimationImg title="ZackBank" description="" software='React,Tailwind CSS'
+            landingPage={'https://zackbank.netlify.app/'} github={'https://github.com/zacky791/ZackBank'}/>
         </div>
       </div>
       <div className="container flex md:space-x-10 flex-col md:flex-row items-center">
-         <div className={classes.container1}>
-            <img src={bravepic} alt="" className='w-auto h-72' />
-            <AnimationImg title="Brave Or Coward" description="Whoever gets the 100 points first wins !" software="(Html,Css,Javascript)" 
-            landingPage={"https://brave-or-coward.netlify.app/"} github={'https://github.com/zacky791/Brave-Or-Coward-Game'}/>
+        <div className={classes.container1}>
+            <img src={projectpic2} alt="" className='w-auto h-72' />
+            <AnimationImg title="GrabFood" description="" software='React,Tailwind CSS'
+            landingPage={''} github={'https://github.com/zacky791/grabfood-project'}/>
         </div>
         <div className={classes.container1}>
             <img src={spotify} alt="" className='w-auto h-72' />
@@ -53,6 +51,11 @@ function Portfolio() {
             landingPage={''} github={''}
             />
         </div>
+                 {/* <div className={classes.container1}>
+            <img src={bravepic} alt="" className='w-auto h-72' />
+            <AnimationImg title="Brave Or Coward" description="Whoever gets the 100 points first wins !" software="(Html,Css,Javascript)" 
+            landingPage={"https://brave-or-coward.netlify.app/"} github={'https://github.com/zacky791/Brave-Or-Coward-Game'}/>
+        </div> */}
       </div>
     </div>
     
